2d6caf881a71dd25de58c8e29a2d2ecaafda38de
Mathieu M <math13.mauger@gmail.com>
2024-10-04 12:07:56 +0200

    last commit fav product

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -4,17 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NotificationController extends AbstractController
 {
     #[Route('/notification', name: 'app_notification')]
-    public function notification(Product $product): Response
+    public function notification(int $id, EntityManagerInterface $entityManager): Response
     {
-        $user = $this->getUser();
-
-        $seller = $product->getSeller();
+        
 
         return $this->render('notification/index.html.twig', [
             
