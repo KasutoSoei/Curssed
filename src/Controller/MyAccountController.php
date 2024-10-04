2e4cd49f896562ba71add37653ef8b22f89a8cbe
Lina <lina.meniai@edu.esiee-it.fr>
2024-10-04 13:16:21 +0200

    touch up

--- src/Controller/MyAccountController.php
+++ src/Controller/MyAccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Transaction;
 use Doctrine\ORM\EntityManager;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,11 +15,15 @@ class MyAccountController extends AbstractController
     #[Route('/myAccount', name: 'app_my_account')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-
         $user = $this->getUser();
+        $transactionsBuyer = $entityManager->getRepository(Transaction::class)->findByBuyer($user->getId());
+        $transactionsSeller = $entityManager->getRepository(Transaction::class)->findBySeller($user->getId());
+        //dd($transactionsSeller);
 
         return $this->render('my_account/index.html.twig', [
             'user' => $user,
+            'transactionsBuyer' => $transactionsBuyer,
+            'transactionsSeller' => $transactionsSeller,
         ]);
     }
 }
